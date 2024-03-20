11f03835ab89f6f858f13468903de27c947609f8
NicolasUdea <nicolas.vargasf@udea.edu.co>
2024-03-20 11:17:42 -0500

    Punto 1 completo

--- main_script.m
+++ main_script.m
@@ -28,17 +28,33 @@ tipos = varfun(@class,datos,'OutputFormat','cell');
 tabla_tipos = table(variables', tipos', 'VariableNames', {'NombreVariable', 'Tipo'});
 
 for i = 1:length(variables)
-    disp(['Tabla de frecuencias para ', variables{i}])
     if ismember(variables{i}, {'height', 'weight', 'age', 'tea', 'coffee'})
         % Para variables continuas, muestra estadísticas descriptivas y un histograma
-        disp(min(datos.(variables{i})));
-        disp(max(datos.(variables{i})));
-        disp(mean(datos.(variables{i})));
-        disp(median(datos.(variables{i})));
+        disp(['**Tabla de frecuencias para la variable ', variables{i}, ':**'])
+        disp(['Valor mínimo de ', variables{i}, ': ', num2str(min(datos.(variables{i})))]);
+        disp(['Valor máximo de ', variables{i}, ': ', num2str(max(datos.(variables{i})))]);
+        disp(['Media de ', variables{i}, ': ', num2str(mean(datos.(variables{i})))]);
+        disp(['Mediana de ', variables{i}, ': ', num2str(median(datos.(variables{i})))]);
+        figure; % Crea una nueva figura
         histogram(datos.(variables{i}));
+        title(['Histograma de ', variables{i}]); % Agrega un título al gráfico
+        xlabel(variables{i}); % Etiqueta el eje x
+        ylabel('Frecuencia'); % Etiqueta el eje y
     else
         % Para variables categóricas, muestra una tabla de frecuencias y un gráfico de barras
-        disp(tabulate(datos.(variables{i})))
-        bar(tabulate(datos.(variables{i})))
+        tab = tabulate(datos.(variables{i}));
+        disp(['**Tabla de frecuencias para la variable ', variables{i}, ':**']);
+        disp('Valores que toma la variable ');
+        disp(tab(:,1));
+        disp('Frecuencia ');
+        disp(tab(:,2));
+        disp('Frecuencia relativa (%) ');
+        disp(tab(:,3));
+        figure; % Crea una nueva figura
+        bar(tab(:,2))
+        title(['Gráfico de barras de ', variables{i}]); % Agrega un título al gráfico
+        xlabel('Categorías'); % Etiqueta el eje x
+        ylabel('Frecuencia'); % Etiqueta el eje y
+        set(gca, 'XTickLabel',tab(:,1)); % Asigna las etiquetas de las categorías al eje x
     end
 end
